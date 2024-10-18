f1b43436006b2f8f54957b1ac010b52e6c2444c6
LakshyaSharma11x <lakshyasharma11052002@gmail.com>
2024-10-18 22:03:58 +0530

    add var to buttons

--- js/index.js
+++ js/index.js
@@ -1,11 +1,9 @@
 function basicButton(symbol){
     let x = document.getElementById("ans");
-
     x.value = x.value + symbol;
 }
 
 function ansButton(){
     let x = document.getElementById("ans");
-
     x.value = eval(x.value);
 }
